aaa951f2d65e66493447ca8867e87a8fe2962eee
haein-side <kimhi1640@gmail.com>
2024-03-09 15:48:34 +0900

    branch1 commit2

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("test commit 1");
         System.out.println("branch1 commit 1");
+        System.out.println("branch1 commit 2");
     }
 }
