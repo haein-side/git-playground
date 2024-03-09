a64b5a7b09938113c94dc8021b3c284105b80d2f
haein-side <kimhi1640@gmail.com>
2024-03-09 16:14:45 +0900

    RevertTest class 메소드 추가

--- src/RevertTest.java
+++ src/RevertTest.java
@@ -6,4 +6,8 @@ public class RevertTest {
     public void testMethod2() {
         System.out.println("revert try2");
     }
+
+    public void testMethod3() {
+        System.out.println("revert try3");
+    }
 }
