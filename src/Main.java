fb725898d602799c4c649eaf1728aa1151b6177d
haein-side <kimhi1640@gmail.com>
2024-03-14 06:40:39 +0900

    for conflict

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("test commit 1");
         System.out.println("branch1 commit 1");
+        System.out.println("local change");
+        System.out.println("local change");
+        System.out.println("local change");
     }
 }
